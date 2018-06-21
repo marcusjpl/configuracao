9d84898f09826ffe067fb7c06efdc4cae1442b72
marcus <marcus_loureiro@ecad.org.br>
2018-06-21 15:43:28 -0300

    inicializando

---
+++ configuracao/src/main/java/org/ecad/configuracao/ConfiguracaoApplication.java
@@ -0,0 +1,12 @@
+package org.ecad.configuracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracaoApplication.class, args);
+	}
+}
