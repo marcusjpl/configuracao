2b734fa19ae5ee21dee11e4218b7107c6ae42f3a
marcus <marcus_loureiro@ecad.org.br>
2018-06-26 13:28:05 -0300

    layout desconfigurado

--- configuracao/src/main/java/org/ecad/configuracao/controller/HomeController.java
+++ configuracao/src/main/java/org/ecad/configuracao/controller/HomeController.java
@@ -16,13 +16,67 @@ public class HomeController {
 	@RequestMapping("/configuracao")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "welcome";
+		return "index";
+	}
+	
+	@RequestMapping("configuracao/welcome")
+	public String inicio(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/welcome.html";
+	}
+	
+	@RequestMapping("configuracao/forms.html")
+	public String forms(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/forms";
+	}
+	
+	@RequestMapping("configuracao/tables.html")
+	public String tables(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/tables";
+	}
+	
+	@RequestMapping("configuracao/grid.html")
+	public String grid(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/grid";
+	}
+	
+	@RequestMapping("configuracao/login.html")
+	public String login(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/login";
+	}
+	
+	@RequestMapping("configuracao/buttons.html")
+	public String buttons(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/buttons";
+	}
+	
+	@RequestMapping("configuracao/notifications.html")
+	public String notifications(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/notifications";
+	}
+	
+	@RequestMapping("configuracao/panels-wells.html")
+	public String panels(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/panels-wells";
+	}
+	
+	@RequestMapping("configuracao/icons.html")
+	public String icons(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "configuracao/icons";
 	}
 	
 	@RequestMapping("/")
 	public String index(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "welcome";
+		return "index";
 	}
 
 	@RequestMapping("/swagger")
