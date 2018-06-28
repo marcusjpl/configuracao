9c7981cbb37dcd7b281a24636d65f88f92c2a6e0
marcus <marcus_loureiro@ecad.org.br>
2018-06-28 11:13:36 -0300

    organizando

--- configuracao/src/main/resources/static/controller/ambienteController.js
+++ configuracao/src/main/resources/static/controller/ambienteController.js
@@ -1,15 +1,9 @@
 app.controller("ambienteController", function($scope, $http) {
 	
-    $scope.firstName = "Marcus";
-    $scope.lastName= "Teste";
-    
-    
     $scope.sistema= {};
-    
     
     $scope.sistemas = [{nome:'SGA', descricao:'descricao sga'}, 
     				   {nome:'SGI', descricao:'descricao sgi'}];
-    
     
     
     $scope.limpar = function() {
