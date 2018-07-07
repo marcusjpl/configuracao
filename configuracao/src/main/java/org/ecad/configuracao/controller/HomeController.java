bc8226bb6156ca15ccaee62e8893b1137ed5439e
Marcus <Marcus@192.168.0.15>
2018-07-07 14:21:03 -0300

    Organizando

--- configuracao/src/main/java/org/ecad/configuracao/controller/HomeController.java
+++ configuracao/src/main/java/org/ecad/configuracao/controller/HomeController.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -13,87 +13,87 @@ public class HomeController {
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
-	@RequestMapping("/configuracao")
+	@GetMapping("/configuracao")
 	public String welcome(Map<String, Object> model) {
 		return "index";
 	}
 	
-	@RequestMapping("/view/sistema.html")
+	@GetMapping("/view/sistema.html")
 	public String sistema(Map<String, Object> model) {
 		return "view/sistema.html";
 	}
 	
-	@RequestMapping("/view/ambiente.html")
+	@GetMapping("/view/ambiente.html")
 	public String ambiente(Map<String, Object> model) {
 		return "view/ambiente.html";
 	}
 	
-	@RequestMapping("/view/propriedade.html")
+	@GetMapping("/view/propriedade.html")
 	public String propriedade(Map<String, Object> model) {
 		return "view/propriedade.html";
 	}
 	
-	@RequestMapping("/view/inicial.html")
+	@GetMapping("/view/inicial.html")
 	public String inicial(Map<String, Object> model) {
 		return "view/inicial.html";
 	}
 	
-	@RequestMapping("/menu_vertical.html")
+	@GetMapping("/menu_vertical.html")
 	public String menuVertical(Map<String, Object> model) {
 		return "includes/menu_vertical.html";
 	}
 	
-	@RequestMapping("/menu_horizontal.html")
+	@GetMapping("/menu_horizontal.html")
 	public String menuHorizontal(Map<String, Object> model) {
 		return "includes/menu_horizontal.html";
 	}
 	
-	@RequestMapping("configuracao/forms.html")
+	@GetMapping("configuracao/forms.html")
 	public String forms(Map<String, Object> model) {
 		return "configuracao/forms";
 	}
 	
-	@RequestMapping("configuracao/tables.html")
+	@GetMapping("configuracao/tables.html")
 	public String tables(Map<String, Object> model) {
 		return "configuracao/tables";
 	}
 	
-	@RequestMapping("configuracao/grid.html")
+	@GetMapping("configuracao/grid.html")
 	public String grid(Map<String, Object> model) {
 		return "configuracao/grid";
 	}
 	
-	@RequestMapping("configuracao/login.html")
+	@GetMapping("configuracao/login.html")
 	public String login(Map<String, Object> model) {
 		return "configuracao/login";
 	}
 	
-	@RequestMapping("configuracao/buttons.html")
+	@GetMapping("configuracao/buttons.html")
 	public String buttons(Map<String, Object> model) {
 		return "configuracao/buttons";
 	}
 	
-	@RequestMapping("configuracao/notifications.html")
+	@GetMapping("configuracao/notifications.html")
 	public String notifications(Map<String, Object> model) {
 		return "configuracao/notifications";
 	}
 	
-	@RequestMapping("configuracao/panels-wells.html")
+	@GetMapping("configuracao/panels-wells.html")
 	public String panels(Map<String, Object> model) {
 		return "configuracao/panels-wells";
 	}
 	
-	@RequestMapping("configuracao/icons.html")
+	@GetMapping("configuracao/icons.html")
 	public String icons(Map<String, Object> model) {
 		return "configuracao/icons";
 	}
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index(Map<String, Object> model) {
 		return "configuracao/";
 	}
 
-	@RequestMapping("/swagger")
+	@GetMapping	("/swagger")
 	public String home() {
 		return "redirect:/swagger-ui.html";
 	}
