9649ca4b7b8e89ca931ddde53dd51f27380745fc
marcus <marcus_loureiro@ecad.org.br>
2018-08-08 11:50:28 -0300

    Adicionando CORS

--- configuracao/src/main/java/org/ecad/configuracao/controller/SistemaController.java
+++ configuracao/src/main/java/org/ecad/configuracao/controller/SistemaController.java
@@ -7,6 +7,7 @@ import org.ecad.configuracao.service.SistemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin({"http://localhost:3000", "http://localhost:7070"})
 @RequestMapping("/api")
 public class SistemaController {
 
